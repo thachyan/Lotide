b6b86e79037c3082d7f9b60f32df54bc233716c9
Randy Thach-Yang <thachyan@ualberta.ca>
2021-06-23 06:04:09 +0000

    Insert function for tail of array

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+
+
+
+
