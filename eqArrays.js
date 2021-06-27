c945af5a89cde32436ca981ebc04c2fde1fcabd0
Randy Thach-Yang <thachyan@ualberta.ca>
2021-06-27 06:54:48 +0000

    Insert eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Passed: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  } return true;
+}
+
